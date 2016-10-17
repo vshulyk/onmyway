4b341b86636cb9198f86dd9e294f15c8b95947fb
vshulyk <v.shulyk@gmail.com>
2016-10-17 22:48:50 +0300

    eslint

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -8,22 +8,22 @@ import {
 } from '../actions/types';
 
 var defaultState = {
-	name:'', 
-	value:'',
-	modal: false
-}
+    name: '',
+    value: '',
+    modal: false
+};
 
 export default function UserReducer(state = defaultState, action) {
     switch(action.type) {
-	    case SET_USER_VALUE:
-	        return {...state, value: action.payload};
-	    case SET_USER_NAME:
-	        return {...state, name: action.payload};
-	    case OPEN_USERNAME_MODAL:
-	        return {...state, modal: action.payload};
-	    case CLOSE_USERNAME_MODAL:
-	        return {...state, modal: action.payload};
-	    default:
-	        return state;
+    case SET_USER_VALUE:
+        return {...state, value: action.payload};
+    case SET_USER_NAME:
+        return {...state, name: action.payload};
+    case OPEN_USERNAME_MODAL:
+        return {...state, modal: action.payload};
+    case CLOSE_USERNAME_MODAL:
+        return {...state, modal: action.payload};
+    default:
+        return state;
     }
 }
